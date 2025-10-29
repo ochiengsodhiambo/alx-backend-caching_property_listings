185a4071d48563d917a4f04ef753c82bc2733755
Ochieng Odhiambo <chiengsun@gmail.com>
2025-10-29 19:30:15 +0300

    Update views.py

--- properties/views.py
+++ properties/views.py
@@ -3,11 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
+from django.core.exceptions import ObjectDoesNotExist
 from .models import Property
-from .utils import get_all_properties  # we'll create this next
+from .utils import get_all_properties
 
 @cache_page(60 * 15)
 def property_list(request):
-    properties = get_all_properties()
-    data = list(properties.values())
-    return JsonResponse(data, safe=False)
+    try:
+        properties = get_all_properties()
+        data = list(properties.values())
+        return JsonResponse({"count": len(data), "results": data})
+    except ObjectDoesNotExist:
+        return JsonResponse({"error": "No properties found"}, status=404)
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=500)
+
